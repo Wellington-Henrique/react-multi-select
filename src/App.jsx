ec7ea541044b374870dc0ac5daf00ca985c9084b
Wellington Henrique <wellingtonhlc@gmail.com>
2024-06-15 11:14:54 -0300

    feat: added wrapp-container

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import { MultiSelect } from "./MultiSelect";
 const dt = [
   { value: "1", label: "um" },
   { value: "2", label: "dois" },
+  { value: "3", label: "três" },
+  { value: "4", label: "quatro" },
+  { value: "5", label: "cinco" },
+  { value: "6", label: "seis" },
+  { value: "7", label: "set" },
+  { value: "8", label: "oito" },
 ];
 
 function App() {
   return (
-    <div style={{ width: "350px" }}>
-      <MultiSelect data={dt} />
+    <div style={{ width: "350px", height: "100px", overflow: "hidden" }}>
+      <MultiSelect data={dt} setData={() => {}} />
     </div>
   );
 }
--- src/MultiSelect/index.jsx
+++ src/MultiSelect/index.jsx
@@ -2,60 +2,87 @@ import { useEffect, useState } from "react";
 
 import "./styles.css";
 
-export function MultiSelect({ data = ["no-data"], setData, ...props }) {
-  const [currentData, setCurrentData] = useState(
-    data.map((i) => i.value.toString())
-  );
+export function MultiSelect({ data = [], setData, ...props }) {
+  const [currentData, setCurrentData] = useState(data.map((i) => i.value));
 
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
+    if (isOpen) {
+      const selectHeader = document.querySelector(".select-header");
+      const selectContent = document.querySelector(".select-content");
+
+      const headerWidth = selectHeader.offsetWidth;
+
+      selectContent.style.width = `${headerWidth}px`;
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
     setData(currentData);
-  }, []);
+  }, [currentData]);
 
   const handleSelect = (value) => {
     const item = currentData.find((i) => i === value);
 
-    let tempList = currentData.filter((i) => i !== "no-data");
+    const tempList = currentData.filter((i) => i !== value);
 
-    if (item) tempList = tempList.filter((i) => i !== value.toString());
-    else tempList = [...tempList, value.toString()];
+    if (!item) tempList.push(value);
 
-    if (tempList.length) setCurrentData(tempList);
-    else {
-      setCurrentData(["no-data"]);
-    }
+    setCurrentData(tempList);
+  };
+
+  const handleSelectAll = () => {
+    setCurrentData(data.map((i) => i.value));
   };
 
   const toggle = (e) => {
-    e.stopPropagation();
+    e?.stopPropagation();
     setIsOpen(!isOpen);
   };
 
   return (
     <>
-      <div className="select-container" {...props} onClick={toggle}>
-        <div className="select-data">
-          {currentData.length
-            ? `${currentData.length} selecionado(s)`
-            : "Selecione..."}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={isOpen ? "select-content-is-visible" : ""}
-            viewBox="0 0 512 512"
-          >
-            <path
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="48"
-              d="M112 184l144 144 144-144"
-            />
-          </svg>
+      <div className="select-wrapper">
+        <div className="select-container" {...props} onClick={toggle}>
+          <div className="select-header">
+            {currentData.length
+              ? `${currentData.length} selecionado(s)`
+              : "Selecione..."}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={isOpen ? "select-content-is-visible" : ""}
+              viewBox="0 0 512 512"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="48"
+                d="M112 184l144 144 144-144"
+              />
+            </svg>
+          </div>
         </div>
+
         {isOpen && (
           <ul className="select-content">
+            <li
+              className={`select-item ${
+                currentData.length === data.length ? "select-item-selected" : ""
+              }`}
+              key="all-tems"
+              onClick={handleSelectAll}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M400 48H112a64.07 64.07 0 00-64 64v288a64.07 64.07 0 0064 64h288a64.07 64.07 0 0064-64V112a64.07 64.07 0 00-64-64zm-35.75 138.29l-134.4 160a16 16 0 01-12 5.71h-.27a16 16 0 01-11.89-5.3l-57.6-64a16 16 0 1123.78-21.4l45.29 50.32 122.59-145.91a16 16 0 0124.5 20.58z" />
+              </svg>
+              Todos
+            </li>
+
+            <hr />
+
             {data.map((item) => (
               <li
                 className={`select-item ${
